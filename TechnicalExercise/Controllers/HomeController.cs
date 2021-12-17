98554ff4b4315b3f01d4355446c5a7c8488c3800
Lindsey Arwine <lindseya@ad.exigo.com>
2021-12-17 10:54:08 -0600

    Add project files.

---
+++ TechnicalExercise/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TechnicalExercise.Models;
+
+namespace TechnicalExercise.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Register()
+        {
+            ViewBag.Message = "Register";
+
+            var model = new CandidateModel();
+            model.PositionList.Add(new SelectListItem { Text = "Web Developer", Value = "1" });
+            model.PositionList.Add(new SelectListItem { Text = "Platform Developer", Value = "2" });
+            model.PositionList.Add(new SelectListItem { Text = "Business Analyst", Value = "3" });
+            model.PositionList.Add(new SelectListItem { Text = "Project Manager", Value = "4" });
+            model.JoinDate = DateTime.Today;
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(CandidateModel model)
+        {
+            ViewBag.Message = "Register";
+
+            return View("Index", model);
+        }
+    }
+}
---
+++ TechnicalExercise/Models/CandidateModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TechnicalExercise.Models
+{
+    public class CandidateModel
+    {
+        public CandidateModel(){
+            PositionList = new List<SelectListItem>();
+        }
+
+        [Display(Name = "First Name")]
+        [Required]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [Required]
+        public string LastName { get; set; }
+
+        [Display(Name = "Position")]
+        [Required]
+        public List<SelectListItem> PositionList { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name = "Join Date")]
+        [Required]
+        public DateTime JoinDate { get; set; }
+
+        [Display(Name = "Salary")]
+        [Required]
+        public double SalaryRange { get; set; }
+
+        [Display(Name = "Are you applying to other companies?")]
+        [Required]
+        public bool ApplyToOtherJobs { get; set; }
+
+    }
+
+}
